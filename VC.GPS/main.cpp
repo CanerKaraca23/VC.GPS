c39a2785eb6263be4e559548dd54c80e762936d6
CanerKaraca23 <37447503+CanerKaraca23@users.noreply.github.com>
2026-07-08 21:27:16 +0000

    Fix MSVC compiler error for injector auto_pointer cast using get_raw
    - Replaced `.get<T>()` with `reinterpret_cast<T>(.get_raw<void>())` to correctly resolve MSVC C2440 pointer-to-function-pointer build failures during the CI workflow check

--- VC.GPS/main.cpp
+++ VC.GPS/main.cpp
@@ -383,7 +383,7 @@ void Init()
     injector::MakeCALL(0x4A4896, InitialiseRadar);
     injector::MakeNOP(0x4C1D49, 5);
 
-    pfDrawInMenu = injector::MakeCALL(0x49E3D9, OnMenuDrawing).get<void(__cdecl *)(float, float, short *)>();
+    pfDrawInMenu = reinterpret_cast<void(__cdecl *)(float, float, short *)>(injector::MakeCALL(0x49E3D9, OnMenuDrawing).get_raw<void>());
 }
 
 void DrawPathFindLineMenuMap()
