d6a0cef182956ca542f4546d81d72d0ee3481ec0
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2026-07-23 21:02:13 +0300

    More Connfigurations for Optimizations

--- premake5.lua
+++ premake5.lua
@@ -1,30 +1,34 @@
 workspace "VC.GPS"
     architecture "x86"
     configurations { "Debug", "Release" }
-    
-    startproject "VC.GPS"
 
 project "VC.GPS"
     kind "SharedLib"
     language "C++"
     cppdialect "C++latest"
     targetextension ".asi"
-    
+
     targetdir ("bin/%{cfg.buildcfg}")
     objdir ("obj/%{cfg.buildcfg}")
 
     files { "VC.GPS/**.cpp", "VC.GPS/**.hpp" }
-    
+
     multiprocessorcompile "On"
     warnings "Extra"
-    buildoptions { "/sdl" }
-    
+
     filter "configurations:Debug"
         defines { "_DEBUG" }
         symbols "On"
 
     filter "configurations:Release"
         defines { "NDEBUG" }
-        optimize "On"
-        staticruntime "On"
+        optimize "Speed"
         floatingpoint "Fast"
+        linktimeoptimization "On"
+        vectorextensions "AVX2"
+        largeaddressaware "On"
+        rtti "Off"
+        exceptionhandling "Off"
+        symbols "Off"
+        omitframepointer "On"
+        buildoptions { "/Gw", "/Qpar", "/Zc:preprocessor" }
